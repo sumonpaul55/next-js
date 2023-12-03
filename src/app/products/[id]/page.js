fecb4489146379ddbcb3f38a30f18046fa2a8297
sumon paul <sumpaul100@gmail.com>
2023-12-04 00:39:19 +0600

    dynamic route id created

---
+++ src/app/products/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DynamicPage = () => {
+    return (
+        <div>
+            this is dynamic page
+        </div>
+    );
+};
+
+export default DynamicPage;
---
+++ src/app/products/page.js
@@ -0,0 +1,9 @@
+const Productspage = () => {
+    return (
+        <div>
+            <h1 className="text-8xl font-bold text-center mt-10">this is prodcuct</h1>
+        </div>
+    );
+};
+
+export default Productspage;
