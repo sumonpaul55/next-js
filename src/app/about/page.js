2648a68be9b838cea6c1771e71c341da8cfafb37
sumon paul <sumpaul100@gmail.com>
2023-12-04 00:31:13 +0600

    folder base route implements

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h2>this is About page</h2>
+
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            Contact us page
+        </div>
+    );
+};
+
+export default ContactPage;
