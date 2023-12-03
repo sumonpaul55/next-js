cbb099cf0f011d8daa73e558317e9daf112d3004
sumon paul <sumpaul100@gmail.com>
2023-12-04 00:51:04 +0600

    nested route blog and article folder inside news folder

---
+++ src/app/news/article/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticalePage = () => {
+    return (
+        <div>
+            this is article page
+        </div>
+    );
+};
+
+export default ArticalePage;
