e8c9a5eccef2ae94c571e0d51cae3fadab294790
sumon paul <sumpaul100@gmail.com>
2023-12-04 23:22:51 +0600

    next  meta data for seo

--- src/app/(withLayout)/about/page.js
+++ src/app/(withLayout)/about/page.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
+export const metadata = {
+    title: "About page",
+    description: "this is about page"
+};
 const AboutPage = () => {
+
     return (
         <div>
             <h1>This is about page with header footer</h1>
