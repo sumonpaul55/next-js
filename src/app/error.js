b9fc266834c92f18a64dd5c43c215ceb964f36e5
sumon paul <sumpaul100@gmail.com>
2023-12-04 22:53:57 +0600

    error

---
+++ src/app/error.js
@@ -0,0 +1,11 @@
+"use client"
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Something went wrog</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import Counter from "@/components/counter/Counter";
 
 const HomePage = () => {
   // console.log(counter)
+  throw new Error()
   return (
     <div>
+      <h1>This is Home page</h1>
       <Counter />
     </div>
   );
